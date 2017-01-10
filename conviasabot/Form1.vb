772a726a08197f114cfd4cc176073f09de8682d0
juanche8 <juanjosediazyoris@gmail.com>
2017-01-10 16:01:58 -0300

    optimizacion

--- conviasabot/Form1.vb
+++ conviasabot/Form1.vb
@@ -2,28 +2,21 @@
 Imports System.IO
 Imports System.Text.RegularExpressions
 Imports System.Windows.Forms.VisualStyles.VisualStyleElement.ListView
-
 Public Class Form1
-
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         TabNavigationPage1.PageText = "Email"
         TabNavigationPage2.PageText = "Fechas"
         TabNavigationPage3.PageText = "index.js"
     End Sub
-
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim reader As New StreamReader("C:\Users\juanyoris\Documents\GitHub\conviasa-bot\config2.js") 'directorio de prueba. podra ser un reader global.
         Dim content As String = reader.ReadToEnd()
         reader.Close()
-       
-        Dim replacetext = vbTab & vbtab & "to: '" & mailbox.Text & "',"
+               Dim replacetext = vbTab & vbtab & "to: '" & mailbox.Text & "',"
         dim primero = content.substring(191)
         Dim lineas() As String = File.ReadAllLines("C:\Users\juanyoris\Documents\GitHub\conviasa-bot\config2.js")
-        Label1.Text = lineas(6)
-        content = Regex.Replace(content, lineas(6), replacetext)
-        
-
-        Dim writer As New StreamWriter("C:\Users\juanyoris\Documents\GitHub\conviasa-bot\config2.js")
+                content = Regex.Replace(content, lineas(6), replacetext)
+               Dim writer As New StreamWriter("C:\Users\juanyoris\Documents\GitHub\conviasa-bot\config2.js")
         writer.Write(content)
         writer.Close() ' comments
          End Sub
@@ -33,18 +26,17 @@ Public Class Form1
         Dim emailMatch As System.Text.RegularExpressions.Match =
            emailRegex.Match(email)
         Return emailMatch.Success
-    End Function
-
-    
+    End Function  
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         Dim reader As New StreamReader("C:\Users\juanyoris\Documents\GitHub\conviasa-bot\config2.js")
         Dim content As String = reader.ReadToEnd()
         reader.Close()
-        Dim fecha1 =vbTab+vbTab+vbTab+ """B_DATE_1"": """  + Format(DateEdit1.DateTime, "yyyyMMdd1200")+""""+","
-        Dim fecha2 =vbTab+vbTab+vbTab+ """B_DATE_2"": """  + Format(DateEdit2.DateTime, "yyyyMMdd0000")+""""+","
-        Dim fecha3 =vbTab+vbTab+vbTab+ """fecha_desde"": """  +DateEdit1.Text+""""+","
-        Dim fecha4 =vbTab+vbTab+vbTab+ """fecha_hasta"": """  +DateEdit2.text+""""+","
+        Dim tab = vbTab+vbTab+vbTab
+        Dim fecha1 =tab+ """B_DATE_1"": """  + Format(DateEdit1.DateTime, "yyyyMMdd1200")+""""+","
+        Dim fecha2 =tab+ """B_DATE_2"": """  + Format(DateEdit2.DateTime, "yyyyMMdd0000")+""""+","
+        Dim fecha3 =tab+ """fecha_desde"": """  +DateEdit1.Text+""""+","
+        Dim fecha4 =tab+ """fecha_hasta"": """  +DateEdit2.text+""""+","
         Dim lineas() As String = File.ReadAllLines("C:\Users\juanyoris\Documents\GitHub\conviasa-bot\config2.js")
         
         content = Regex.Replace(content, lineas(40), fecha1)
@@ -55,11 +47,6 @@ Public Class Form1
         writer.Write(content)
         writer.Close()
        ' Format(DateEdit2.DateTime, "yyyyMMdd0000")
-        Label4.Text = "DESDE EL DIA "+ DateEdit1.Text + " HASTA " + DateEdit2.DateTime
-                   
-    End Sub
-
-    Private Sub TabNavigationPage2_Paint(sender As Object, e As PaintEventArgs) Handles TabNavigationPage2.Paint
-
+        Label4.Text = "DESDE EL DIA "+ DateEdit1.Text + " HASTA " + DateEdit2.DateTime  
     End Sub
 End Class
